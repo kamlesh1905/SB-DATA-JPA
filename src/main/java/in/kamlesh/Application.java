26b8ea529dac3b103061950a9e35db231de369ee
kamlesh <kamlesh.merugu30@gmail.com>
2021-03-20 16:08:23 +0800

    Commit

--- src/main/java/in/kamlesh/Application.java
+++ src/main/java/in/kamlesh/Application.java
@@ -1,5 +1,9 @@
 package in.kamlesh;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -19,15 +23,63 @@ public class Application {
     System.out.println(dtlsRepository.getClass().getName());
     
     UserDtls user = new UserDtls();
-    user.setUserId(2);
-    user.setUsername("Ganesh");
-    user.setUserEmail("G@gmail.com");
+    
+    UserDtls user1 = new UserDtls();
+    
+    user.setUserId(4);
+    user.setUsername("Kumar");
+    user.setUserEmail("Kum@gmail.com");
     user.setUserAge(27);
     user.setUserCount("USA");
     
-    dtlsRepository.save(user);
+    user1.setUserId(5);
+    user1.setUsername("Rahesh");
+    user1.setUserEmail("rahes@gmail.com");
+    user1.setUserAge(27);
+    user1.setUserCount("USA");
     
-    System.out.println("Record Saved");
+    // save
+    
+    // dtlsRepository.save(user);
+    
+    List<UserDtls> list = new ArrayList<>();
+    list.add(user);
+    list.add(user1);
+    
+    // saveall
+    
+    //dtlsRepository.saveAll(list);
+    
+    //System.out.println("All Record Saved");
+    
+    
+    //Optional<UserDtls> findById = dtlsRepository.findById(1);
+    
+   // if(findById.isPresent())
+   // {
+   // 	UserDtls userdtls = findById.get();
+    	
+    //	 System.out.println (userdtls); 
+    	 
+    //}
+     
+    
+    Iterable<UserDtls> findAll=dtlsRepository.findAll();
+    findAll.forEach(user2 -> {
+    	
+    	System.out.println(user2);
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
     
 	}
 
