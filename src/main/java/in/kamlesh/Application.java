505cf4731269c0162ae0ae739a3f276353ab53d9
Kamlesh merugu <Kamlesh merugu@DESKTOP-8HGJPAF>
2021-03-21 16:43:54 +0800

    Third commit

--- src/main/java/in/kamlesh/Application.java
+++ src/main/java/in/kamlesh/Application.java
@@ -1,5 +1,6 @@
 package in.kamlesh;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
@@ -19,52 +20,50 @@ public class Application {
 
 		System.out.println(dtlsRepository.getClass().getName());
 
-		UserDtls user = new UserDtls();
+		/*
+		 * UserDtls user = new UserDtls();
+		 * 
+		 * UserDtls user1 = new UserDtls();
+		 * 
+		 * UserDtls user2 = new UserDtls();
+		 * 
+		 * UserDtls user3 = new UserDtls();
+		 */
 
-		UserDtls user1 = new UserDtls();
+		/*
+		 * user.setUserId(6); user.setUsername("Thabi");
+		 * user.setUserEmail("T@gmail.com"); user.setUserAge(28);
+		 * user.setUserCount("USA");
+		 */
 
-		UserDtls user2 = new UserDtls();
-
-		UserDtls user3 = new UserDtls();
-
-		user.setUserId(1);
-		user.setUsername("Karan");
-		user.setUserEmail("Karan@gmail.com");
-		user.setUserAge(25);
-		user.setUserCount("USA");
-
-		user1.setUserId(2); 	
-		user1.setUsername("Rahesh");
-		user1.setUserEmail("rahes@gmail.com");
-		user1.setUserAge(27);
-		user1.setUserCount("India");
-
-		user2.setUserId(3);
-		user2.setUsername("Kumar");
-		user2.setUserEmail("Kum@gmail.com");
-		user2.setUserAge(27);
-		user2.setUserCount("USA");
-
-		user3.setUserId(4);
-		user3.setUsername("Rahesh");
-		user3.setUserEmail("rahes@gmail.com");
-		user3.setUserAge(27);
-		user3.setUserCount("USA");
+		/*
+		 * user1.setUserId(2); user1.setUsername("Rahesh");
+		 * user1.setUserEmail("rahes@gmail.com"); user1.setUserAge(27);
+		 * user1.setUserCount("India");
+		 * 
+		 * user2.setUserId(3); user2.setUsername("Kumar");
+		 * user2.setUserEmail("Kum@gmail.com"); user2.setUserAge(27);
+		 * user2.setUserCount("USA");
+		 * 
+		 * user3.setUserId(4); user3.setUsername("Rahesh");
+		 * user3.setUserEmail("rahes@gmail.com"); user3.setUserAge(27);
+		 * user3.setUserCount("USA");
+		 */
 
 		// save
 
-		// dtlsRepository.save(user);
-		
-		// System.out.println("Single Record Saved");
+		/*
+		 * dtlsRepository.save(user);
+		 * 
+		 * System.out.println("Single Record Saved");
+		 */
 
-	
 		/*
 		 * List<UserDtls> list = new ArrayList<>(); list.add(user); list.add(user1);
 		 * list.add(user2); list.add(user3);
 		 */
-		  
-		 
-		//saveall
+
+		// saveall
 
 		/*
 		 * dtlsRepository.saveAll(list);
@@ -72,8 +71,6 @@ public class Application {
 		 * System.out.println("All Record Saved");
 		 */
 
-		
-		
 		/*
 		 * Optional<UserDtls> findById = dtlsRepository.findById(1);
 		 * 
@@ -83,36 +80,45 @@ public class Application {
 		 * 
 		 * }
 		 */
-		 
-		 
 
-		
-		  /*Iterable<UserDtls> findAll= dtlsRepository.findAll(); 
-		  
-		  findAll.forEach(user5 ->
-		  {
-		  
-		  System.out.println(user2); });
-		  
-	     }*/
-		
+		/*
+		 * Iterable<UserDtls> findAll= dtlsRepository.findAll();
+		 * 
+		 * findAll.forEach(user5 -> {
+		 * 
+		 * System.out.println(user2); });
+		 * 
+		 * }
+		 */
+
 		/*
 		 * boolean existsById = dtlsRepository.existsById(1);
 		 * 
 		 * System.out.println("Record exists " + existsById);
 		 */
+
+		/*
+		 * long count = dtlsRepository.count(); System.out.println("Total Count " +
+		 * count);
+		 */
+
+		/*
+		 * UserDtls userval = dtlsRepository.findByUsername("Karan");
+		 * 
+		 * System.out.println(userval);
+		 * 
+		 * List<UserDtls> users = dtlsRepository.findByUserAge(27); users.forEach(user
+		 * ->{ System.out.println(user); });
+		 */
+
+		List<UserDtls> users101 = dtlsRepository.findByUserAgeAndUserCount(27, "USA");
+		users101.forEach(user -> {
+			System.out.println(user);
+		});
 		
-		
-	        long count = dtlsRepository.count();
-	        
-	        System.out.println("Total Count " + count);
-	
-	
-	        
-	        
-	}		
-	
-	       
-	
+		UserDtls c=dtlsRepository.getUser("Kum@gmail.com");
+		System.out.println(c);
+
+	}
 
 }
--- src/main/java/in/kamlesh/Repository/UserDtlsRepository.java
+++ src/main/java/in/kamlesh/Repository/UserDtlsRepository.java
@@ -1,5 +1,8 @@
 package in.kamlesh.Repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +11,14 @@ import in.kamlesh.Entity.UserDtls;
 @Repository
 public interface UserDtlsRepository extends CrudRepository<UserDtls,Integer> {
 
+public UserDtls findByUsername(String uname);
+
+public List<UserDtls> findByUserAge(Integer age);
+
+public List<UserDtls> findByUserAgeAndUserCount(Integer Age , String Country);
+
+@Query("from UserDtls where userEmail=:email")
+public UserDtls getUser(String email);
 
 	
 }
